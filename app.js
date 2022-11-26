00db5f4aa0f599a94bc7a561aef3b84cc3dbe0d8
MustafaKurt98 <mustafakurt98@outlook.com>
2022-11-26 12:09:35 +0300

    write&readJSon

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000
+const arr = []
+const fs = require('fs');
+
+
+for (let i = 0; i < 100; i++) {
+    arr.push({ 'id': i, 'name': 'name' + i })
+}
+
+app.get('/', (req, res) => {
+    fs.readFile('data.json', (err, data) => {
+        res.send(data)
+
+        if (err) throw err;
+        console.log('Data written to file');
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
