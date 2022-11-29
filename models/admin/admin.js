6bf5383f2b4cef96f4aa787411906084c4875443
MustafaKurt98 <mustafakurt98@outlook.com>
2022-11-29 09:37:42 +0300

    gitignore

--- models/admin/admin.js
+++ models/admin/admin.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 const adminSchema = new mongoose.Schema({
-    email: {
+    nameSurname: {
         type: String,
-    },
-    password: {
+      },
+      identificationNo: {
+        type: Number,
+      },
+      email: {
         type: String,
-    },
+      },
+      password: {
+        type: String,
+      },
+      token:{
+        type:String
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+
 })
 
 module.exports = mongoose.model('admin', adminSchema);
