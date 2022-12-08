4a286fd7e6616751187d38815b177edce09e7033
MustafaKurt98 <mustafakurt98@outlook.com>
2022-12-08 20:22:30 +0300

    new remove method

--- controller/menu/menu_delete.js
+++ controller/menu/menu_delete.js
@@ -10,8 +10,38 @@ const YemekyanindaModel = require('../../models/menu/yemekyaninda-model');
 
 
 const alkoller = async (req, res) => {
-    const { _id, groupName, altGroupName } = req.body;
-    if (!_id || !groupName) {
+    const { _id,objectID , categoryName, subCategoryName } = req.body;
+    if (!_id || !objectID || !categoryName) {
+        res.status(400).json({ message: 'id is required' });
+    } else {
+        const item = await AlkollerModel.findById(_id);
+        if (subCategoryName) {
+            const subItem = item[categoryName][subCategoryName].id(objectID);
+            if (subItem) {
+                subItem.remove();
+                item.save();
+                res.status(200).json({ message: "success" });
+            } else {
+                res.status(400).json({ message: "error" });
+            }
+        } else {
+            const subItem = item[categoryName].id(objectID);
+            if (subItem) {
+                subItem.remove();
+                item.save();
+                res.status(200).json({ message: "success" });
+            } else {
+                res.status(400).json({ message: "error" });
+            }
+        }
+    }
+
+
+
+
+
+
+    /* if (!_id || !groupName) {
         res.status(400).json({ message: 'id is required' });
     } else if (_id != "" && groupName != "") {
         await AlkollerModel.find()
@@ -22,14 +52,14 @@ const alkoller = async (req, res) => {
                             item[groupName][altGroupName].map((item2, index) => {
                                 if (item2._id == _id) {
                                     item2.remove();
-                                } 
+                                }
                             })
                             item.save();
                         } else {
                             item[groupName].map((item, index) => {
                                 if (item._id == _id) {
                                     item.remove();
-                                } 
+                                }
                             })
                             item.save();
                         }
@@ -38,7 +68,7 @@ const alkoller = async (req, res) => {
                         item.map((item, index) => {
                             if (item._id == _id) {
                                 item.remove();
-                            } 
+                            }
                         })
                         item.save();
                     }
@@ -52,18 +82,18 @@ const alkoller = async (req, res) => {
             })
     } else {
         res.status(400).json({ message: 'id is required' });
-    }
+    } */
 }
 
 
 
 const anayemek = async (req, res) => {
-    const { _id } = req.body;
+    const { _id, categoryName } = req.body;
     const anayemekData = await AnayemekModel.find()
     anayemekData.map((item, index) => {
-        let anayemekler = item["ANA YEMEKLER"]
+        let anayemekler = item[categoryName]
         try {
-            anayemekler.findByIdAndDelete({_id:_id}, (err, data) => {
+            anayemekler.findByIdAndDelete({ _id: _id }, (err, data) => {
                 if (err) {
                     console.log(err);
                     res.status(500).json({ message: "error1" });
@@ -79,27 +109,82 @@ const anayemek = async (req, res) => {
 }
 
 const baslangiclar = async (req, res) => {
-    const { _id } = req.body;
-    const baslangicData = await BaslangicModel.findByIdAndDelete(id);
-    res.status(200).json(baslangicData);
+    const { _id, objectID, categoryName } = req.body;
+    if (!_id || !objectID || !categoryName) {
+        res.status(400).json({ message: 'id is required' });
+    } else {
+
+        const item = await BaslangicModel.findById(_id);
+        const subItem = item[categoryName].id(objectID);
+        if (subItem) {
+            subItem.remove();
+            item.save();
+            res.status(200).json({ message: "success" });
+        } else {
+            res.status(400).json({ message: "error" });
+        }
+    }
 }
 
 const burgerdurum = async (req, res) => {
-    const { _id } = req.body;
-    const burgerdurumData = await BurgerDurumModel.findByIdAndDelete(id);
-    res.status(200).json(burgerdurumData);
+    const { _id, objectID, categoryName } = req.body;
+    if (!_id || !objectID || !categoryName) {
+        res.status(400).json({ message: 'id is required' });
+    }
+    const item = await BurgerDurumModel.findById(_id);
+    const subItem = item[categoryName].id(objectID);
+    if (subItem) {
+        subItem.remove();
+        item.save();
+        res.status(200).json({ message: "success" });
+    } else {
+        res.status(400).json({ message: "error" });
+    }
 }
 
 const icecekler = async (req, res) => {
-    const { _id } = req.body;
-    const iceceklerData = await IceceklerModel.findByIdAndDelete(id);
-    res.status(200).json(iceceklerData);
+    const { _id, objectID, categoryName } = req.body;
+    if (!_id || !objectID || !categoryName) {
+        res.status(400).json({ message: 'id is required' });
+    } else {
+        const item = await IceceklerModel.findById(_id);
+        const subItem = item[categoryName].id(objectID);
+        if (subItem) {
+            subItem.remove();
+            item.save();
+            res.status(200).json({ message: "success" });
+        } else {
+            res.status(400).json({ message: "error" });
+        }
+    }
 }
 
 const kokteyller = async (req, res) => {
-    const { _id } = req.body;
-    const kokteyllerData = await KokteyllerModel.findByIdAndDelete(id);
-    res.status(200).json(kokteyllerData);
+    const { _id, objectID, categoryName, subCategoryName } = req.body;
+    if (!_id || !objectID || !categoryName) {
+        res.status(400).json({ message: 'id is required' });
+    } else {
+        const item = await KokteyllerModel.findById(_id);
+        if (subCategoryName) {
+            const subItem = item[categoryName][subCategoryName].id(objectID);
+            if (subItem) {
+                subItem.remove();
+                item.save();
+                res.status(200).json({ message: "success" });
+            } else {
+                res.status(400).json({ message: "error" });
+            }
+        } else {
+            const subItem = item[categoryName].id(objectID);
+            if (subItem) {
+                subItem.remove();
+                item.save();
+                res.status(200).json({ message: "success" });
+            } else {
+                res.status(400).json({ message: "error" });
+            }
+        }
+    }
 }
 
 const pizzamakarna = async (req, res) => {
